b997d0c168bb60b84d3afe69c4725cf7a425438f
Allysson <allyssonmgs@gmail.com>
2023-01-22 16:21:50 -0300

    feat: ensure CreateUser calls LoadUserRepository with correct params

---
+++ src/domain/contracts/repositories/user.ts
@@ -0,0 +1,17 @@
+export interface LoadUserRepository {
+  load: (input: LoadUserRepository.Input) => Promise<LoadUserRepository.Output>;
+}
+
+export namespace LoadUserRepository {
+  export type Input = {
+    email: string;
+  };
+
+  export type Output = {
+    id: string,
+    name: string,
+    email: string,
+    cpf: string,
+    password: string,
+    role: string } | undefined;
+}
