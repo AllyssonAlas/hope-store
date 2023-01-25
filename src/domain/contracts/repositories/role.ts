9b95d09903766d3736e85aa422e2150ef9e69dbb
Allysson <allyssonmgs@gmail.com>
2023-01-25 19:12:12 -0300

    feat: ensure CreateUser calls LoadRoleRepository with correct input

---
+++ src/domain/contracts/repositories/role.ts
@@ -0,0 +1,14 @@
+export interface LoadRoleRepository {
+  load: (input: LoadRoleRepository.Input) => Promise<LoadRoleRepository.Output>;
+}
+
+export namespace LoadRoleRepository {
+  export type Input = {
+    name: string;
+  };
+
+  export type Output = {
+    id: string,
+    name: string,
+  } | undefined;
+}
--- src/domain/usecases/create-user.ts
+++ src/domain/usecases/create-user.ts
@@ -1,16 +1,21 @@
-import { LoadUserRepository, SaveUserRepository } from '@/domain/contracts/repositories';
+import {
+  LoadUserRepository,
+  SaveUserRepository,
+  LoadRoleRepository,
+} from '@/domain/contracts/repositories';
 import { HasherGenerator } from '@/domain/contracts/gateways';
 import { UserAlreadyExistsError } from '@/domain/errors';
 
-type Setup = (userRepo: LoadUserRepository & SaveUserRepository, hasher: HasherGenerator) => CreateUser
+type Setup = (userRepo: LoadUserRepository & SaveUserRepository, roleRepo: LoadRoleRepository, hasher: HasherGenerator) => CreateUser
 export type CreateUser = (input: Input) => Promise<void>
 type Input = { name: string, email: string, cpf: string, password: string, role: string }
 
-export const setupCreateUser: Setup = (userRepo, hasher) => async (input) => {
+export const setupCreateUser: Setup = (userRepo, roleRepo, hasher) => async (input) => {
   const user = await userRepo.load({ email: input.email });
   if (user) {
     throw new UserAlreadyExistsError();
   }
+  await roleRepo.load({ name: input.role });
   const { ciphertext } = await hasher.generate({ plaintext: input.password });
   await userRepo.save({
     cpf: input.cpf,
