85d1b078e702b4cc1fac39d8ff7ee4c8ce67efcd
Allysson <allyssonmgs@gmail.com>
2023-01-31 21:24:32 -0300

    refactor: isolate sut creation

--- tests/infra/gateways/bcrypt-adapter.spec.ts
+++ tests/infra/gateways/bcrypt-adapter.spec.ts
@@ -5,13 +5,24 @@ import { BcryptAdapter } from '@/infra/gateways';
 jest.mock('bcrypt');
 
 describe('BcryptAdapter', () => {
+  let plaintext: string;
+  let salt: 12;
+  let fakeBcrypt: jest.Mocked<typeof bcrypt>;
+  let sut : BcryptAdapter;
+
+  beforeAll(() => {
+    plaintext = 'any_plaintext';
+    salt = 12;
+    fakeBcrypt = bcrypt as jest.Mocked<typeof bcrypt>;
+  });
+
+  beforeEach(() => {
+    sut = new BcryptAdapter(salt);
+  });
+
   describe('generate', () => {
     it('Should call sign with correct input', async () => {
-      const salt = 12;
-      const fakeBcrypt = bcrypt as jest.Mocked<typeof bcrypt>;
-      const sut = new BcryptAdapter(salt);
-
-      await sut.generate({ plaintext: 'any_plaintext' });
+      await sut.generate({ plaintext });
 
       expect(fakeBcrypt.hash).toHaveBeenCalledWith('any_plaintext', salt);
       expect(fakeBcrypt.hash).toHaveBeenCalledTimes(1);
