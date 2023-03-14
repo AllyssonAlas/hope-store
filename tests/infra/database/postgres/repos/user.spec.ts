551d12a3d608bebab147e756b0faf71ab63521d1
Allysson <allyssonmgs@gmail.com>
2023-03-14 20:09:45 -0300

    test: ensure LoadUserRepository returns an user with an existing email

---
+++ tests/infra/database/postgres/repos/user.spec.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from '@prisma/client';
+
+import { PgUserRepository } from '@/infra/database/postgres/repositories';
+
+describe('PgUserRepository', () => {
+  let prisma: PrismaClient;
+
+  beforeAll(() => {
+    prisma = new PrismaClient();
+  });
+
+  beforeEach(async () => {
+    await prisma.user.deleteMany({});
+  });
+
+  describe('load', () => {
+    it('Should return an user if email exists', async () => {
+      await prisma.user.create({
+        data: {
+          name: 'any_name',
+          cpf: 'any_cpf',
+          email: 'any_existing_email@mail.com',
+          password: 'any_password',
+          role: 'any_role_id',
+        },
+      });
+
+      const sut = new PgUserRepository();
+      const user = await sut.load({ email: 'any_existing_email@mail.com' });
+
+      expect(user?.id).toBeTruthy();
+      expect(user?.name).toBe('any_name');
+      expect(user?.email).toBe('any_existing_email@mail.com');
+      expect(user?.role).toBe('any_role_id');
+    });
+  });
+});
