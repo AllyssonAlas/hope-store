5a0f3873cb1ebc2407cf2fc5be73d446c8c47070
Allysson <allyssonmgs@gmail.com>
2023-04-21 17:59:24 -0300

    test: ensure LoadRoleRepository returns a role with an existing name

---
+++ tests/infra/database/postgres/repos/role.spec.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client';
+
+import { PgRoleRepository } from '@/infra/database/postgres/repositories';
+
+describe('PgRoleRepository', () => {
+  let prisma: PrismaClient;
+  let sut: PgRoleRepository;
+
+  beforeAll(() => {
+    prisma = new PrismaClient();
+  });
+
+  beforeEach(async () => {
+    sut = new PgRoleRepository();
+  });
+
+  afterEach(async () => {
+    await prisma.role.deleteMany({});
+  });
+
+  describe('load', () => {
+    it('Should return an role if name exists', async () => {
+      await prisma.role.create({ data: { name: 'any_role_name' } });
+
+      const role = await sut.load({ name: 'any_role_name' });
+
+      expect(role?.id).toBeTruthy();
+      expect(role?.name).toBe('any_role_name');
+    });
+  });
+});
