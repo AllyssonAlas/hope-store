25bcbb68c0e7fa24219370da8a8898b3de6e89c3
Allysson <allyssonmgs@gmail.com>
2023-04-23 18:17:12 -0300

    test: ensure RequiredEmailValidator returns error if value is not valid

---
+++ tests/application/validation/required-email.spec.ts
@@ -0,0 +1,36 @@
+import { RequiredEmailValidator } from '@/application/validation';
+import { InvalidRequiredParamError } from '@/application/errors';
+
+describe('RequiredStringValidator', () => {
+  it('Should return InvalidRequiredParamError if value is empty', () => {
+    const sut = new RequiredEmailValidator('', 'email');
+
+    const error = sut.validate();
+
+    expect(error).toEqual(new InvalidRequiredParamError('email'));
+  });
+
+  it('Should return InvalidRequiredParamError if value is null', () => {
+    const sut = new RequiredEmailValidator(null as any, 'email');
+
+    const error = sut.validate();
+
+    expect(error).toEqual(new InvalidRequiredParamError('email'));
+  });
+
+  it('Should return InvalidRequiredParamError if value is undefined', () => {
+    const sut = new RequiredEmailValidator(undefined as any, 'email');
+
+    const error = sut.validate();
+
+    expect(error).toEqual(new InvalidRequiredParamError('email'));
+  });
+
+  it('Should return InvalidRequiredParamError if value is invalid', () => {
+    const sut = new RequiredEmailValidator('invalid_email' as any, 'email');
+
+    const error = sut.validate();
+
+    expect(error).toEqual(new InvalidRequiredParamError('email'));
+  });
+});
