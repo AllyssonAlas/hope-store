9a6b94717aa1efb35b7aa0390d420ad687647532
Allysson <allyssonmgs@gmail.com>
2023-04-23 17:53:35 -0300

    test: ensure RequiredStringValidator returns error if value is not valid

---
+++ tests/application/validation/required-string.spec.ts
@@ -0,0 +1,28 @@
+import { RequiredStringValidator } from '@/application/validation';
+import { RequiredParamError } from '@/application/errors';
+
+describe('RequiredStringValidator', () => {
+  it('Should return RequiredParamError if value is empty', () => {
+    const sut = new RequiredStringValidator('', 'any_field_name');
+
+    const error = sut.validate();
+
+    expect(error).toEqual(new RequiredParamError('any_field_name'));
+  });
+
+  it('Should return RequiredParamError if value is null', () => {
+    const sut = new RequiredStringValidator(null as any, 'any_field_name');
+
+    const error = sut.validate();
+
+    expect(error).toEqual(new RequiredParamError('any_field_name'));
+  });
+
+  it('Should return RequiredParamError if value is undefined', () => {
+    const sut = new RequiredStringValidator(undefined as any, 'any_field_name');
+
+    const error = sut.validate();
+
+    expect(error).toEqual(new RequiredParamError('any_field_name'));
+  });
+});
