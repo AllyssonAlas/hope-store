561fb7f7dbdf3ae3406e6a471884e7c557c6e76f
Allysson <allyssonmgs@gmail.com>
2023-04-23 17:56:45 -0300

    feat: ensure RequiredStringValidator returns undefined if value is valid

--- src/application/validation/required-string.ts
+++ src/application/validation/required-string.ts
@@ -3,6 +3,8 @@ import { RequiredParamError } from '@/application/errors';
 export class RequiredStringValidator {
   constructor(private readonly value: string, private readonly fieldName: string) {}
   validate(): Error | undefined {
-    return new RequiredParamError('any_field_name');
+    if (!this.value) {
+      return new RequiredParamError(this.fieldName);
+    }
   }
 }
