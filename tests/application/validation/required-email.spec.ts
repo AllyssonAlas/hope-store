437cf24a803157b9f41eff54281542895f1c9111
Allysson <allyssonmgs@gmail.com>
2023-04-23 18:22:00 -0300

    test: ensure RequiredEmailValidator returns undefined if value is valid

--- tests/application/validation/required-email.spec.ts
+++ tests/application/validation/required-email.spec.ts
@@ -33,4 +33,12 @@ describe('RequiredStringValidator', () => {
 
     expect(error).toEqual(new InvalidRequiredParamError('email'));
   });
+
+  it('Should return undefined if value is valid', () => {
+    const sut = new RequiredEmailValidator('valid_email@mail.com' as any, 'email');
+
+    const error = sut.validate();
+
+    expect(error).toBeUndefined();
+  });
 });
