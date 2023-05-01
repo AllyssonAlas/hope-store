729e3b5342e848da1735591f788d2db50ee21a7d
Allysson <allyssonmgs@gmail.com>
2023-05-01 18:06:02 -0300

    test: ensure ExpressRouter responds with 200 and correct data

--- tests/main/adapters/express-router.spec.ts
+++ tests/main/adapters/express-router.spec.ts
@@ -15,6 +15,10 @@ describe('ExpressRouter', () => {
     req = getMockReq({ body: { any: 'any' } });
     res = getMockRes().res;
     controller = mock();
+    controller.handle.mockResolvedValue({
+      statusCode: 200,
+      data: { data: 'any_data' },
+    });
   });
 
   beforeEach(() => {
@@ -25,5 +29,15 @@ describe('ExpressRouter', () => {
     await sut.adapt(req, res);
 
     expect(controller.handle).toHaveBeenCalledWith({ any: 'any' });
+    expect(controller.handle).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should respond with 200 and valid data', async () => {
+    await sut.adapt(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith({ data: 'any_data' });
+    expect(res.json).toHaveBeenCalledTimes(1);
   });
 });
