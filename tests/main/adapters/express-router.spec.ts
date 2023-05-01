7b32a15ab30b9c3a6f81d13658c4dce53e40f272
Allysson <allyssonmgs@gmail.com>
2023-05-01 17:56:19 -0300

    test: ensure ExpressRouter calls handle with correct request

---
+++ tests/main/adapters/express-router.spec.ts
@@ -0,0 +1,18 @@
+import { getMockReq, getMockRes } from '@jest-mock/express';
+import { mock } from 'jest-mock-extended';
+
+import { Controller } from '@/application/controllers';
+import { ExpressRouter } from '@/main/adapters';
+
+describe('ExpressRouter', () => {
+  it('Should call handle with correct request', async () => {
+    const req = getMockReq({ body: { any: 'any' } });
+    const { res } = getMockRes();
+    const controller = mock<Controller>();
+    const sut = new ExpressRouter(controller);
+
+    await sut.adapt(req, res);
+
+    expect(controller.handle).toHaveBeenCalledWith({ any: 'any' });
+  });
+});
