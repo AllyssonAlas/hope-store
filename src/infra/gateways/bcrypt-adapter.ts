bf1fa0d11c760aaf32b1a3cc4da1b0e893d4f3bd
Allysson <allyssonmgs@gmail.com>
2023-05-01 14:43:48 -0300

    feat: ensure BcryptAdapter generate returns hashed value

--- src/infra/gateways/bcrypt-adapter.ts
+++ src/infra/gateways/bcrypt-adapter.ts
@@ -2,11 +2,12 @@ import bcrypt from 'bcrypt';
 
 import { HasherGenerator } from '@/domain/contracts/gateways';
 
-export class BcryptAdapter {
+export class BcryptAdapter implements HasherGenerator {
   constructor(private readonly salt: number) {
   }
 
-  async generate({ plaintext }: HasherGenerator.Input): Promise<void> {
-    bcrypt.hash(plaintext, this.salt);
+  async generate({ plaintext }: HasherGenerator.Input): Promise<HasherGenerator.Output> {
+    const ciphertext = await bcrypt.hash(plaintext, this.salt);
+    return { ciphertext };
   }
 }
