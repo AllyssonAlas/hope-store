6f595cadda2774a4d4cbd9d3f4593f1ebe7a3b71
Allysson <allyssonmgs@gmail.com>
2023-05-01 18:02:24 -0300

    refactor: isolate sut creation

--- tests/main/adapters/express-router.spec.ts
+++ tests/main/adapters/express-router.spec.ts
@@ -1,16 +1,27 @@
+import { Request, Response } from 'express';
 import { getMockReq, getMockRes } from '@jest-mock/express';
-import { mock } from 'jest-mock-extended';
+import { mock, MockProxy } from 'jest-mock-extended';
 
 import { Controller } from '@/application/controllers';
 import { ExpressRouter } from '@/main/adapters';
 
 describe('ExpressRouter', () => {
-  it('Should call handle with correct request', async () => {
-    const req = getMockReq({ body: { any: 'any' } });
-    const { res } = getMockRes();
-    const controller = mock<Controller>();
-    const sut = new ExpressRouter(controller);
+  let req: Request;
+  let res: Response;
+  let controller: MockProxy<Controller>;
+  let sut: ExpressRouter;
 
+  beforeAll(() => {
+    req = getMockReq({ body: { any: 'any' } });
+    res = getMockRes().res;
+    controller = mock();
+  });
+
+  beforeEach(() => {
+    sut = new ExpressRouter(controller);
+  });
+
+  it('Should call handle with correct request', async () => {
     await sut.adapt(req, res);
 
     expect(controller.handle).toHaveBeenCalledWith({ any: 'any' });
