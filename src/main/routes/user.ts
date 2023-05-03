31b71dac617487aa31ce3da8cb74656cb0628ad2
Allysson <allyssonmgs@gmail.com>
2023-05-03 19:07:40 -0300

    feat: ensure POST /user/create returns 204 on success

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
+import { makeCreateUserController } from '@/main/factories/application/controllers';
+import { adaptExpressRoute as adapt } from '@/main/adapters';
 
 export default (router: Router): void => {
-  router.post('/user/create', (req, res) => {
-    res.send({ data: 'any_data' });
-  });
+  router.post('/user/create', adapt(makeCreateUserController()));
 };
