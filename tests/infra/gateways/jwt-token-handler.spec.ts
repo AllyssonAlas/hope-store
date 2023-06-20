02aadb53760250ff4d23711a09c41a9eea44f5f1
Allysson <allyssonmgs@gmail.com>
2023-06-19 21:35:03 -0300

    test: ensure JwtTokenHandler calls jsonwebtoken sign with correct input

---
+++ tests/infra/gateways/jwt-token-handler.spec.ts
@@ -0,0 +1,41 @@
+import jwt from 'jsonwebtoken';
+
+import { JwtTokenHandler } from '@/infra/gateways';
+
+jest.mock('jsonwebtoken');
+
+describe('JwtTokenHandler', () => {
+  let permissions: string[];
+  let role: string;
+  let id: string;
+  let secret: string;
+  let fakeJwt: jest.Mocked<typeof jwt>;
+  let sut: JwtTokenHandler;
+
+  beforeAll(() => {
+    fakeJwt = jwt as jest.Mocked<typeof jwt>;
+    secret = 'any_secret';
+    id = 'any_id';
+    role = 'any_role';
+    permissions = ['any_permission'];
+  });
+
+  beforeEach(() => {
+    sut = new JwtTokenHandler(secret);
+  });
+
+  describe('generate', () => {
+    let expirationInMs: number;
+
+    beforeAll(() => {
+      expirationInMs = 1000;
+    });
+
+    it('Should call sign with correct input', async () => {
+      await sut.generate({ id, role, permissions, expirationInMs });
+
+      expect(fakeJwt.sign).toHaveBeenCalledWith({ id, role, permissions }, secret, { expiresIn: 1 });
+      expect(fakeJwt.sign).toHaveBeenCalledTimes(1);
+    });
+  });
+});
