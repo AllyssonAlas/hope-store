f5781ddc28703664244c69f784b77a8fcb75cf37
Allysson <allyssonmgs@gmail.com>
2023-06-29 20:46:00 -0300

    test: ensure CreateProductRepository creates a product

---
+++ tests/infra/database/postgres/repos/product.spec.ts
@@ -0,0 +1,43 @@
+import { PrismaClient } from '@prisma/client';
+
+import { PgProductRepository } from '@/infra/database/postgres/repositories';
+
+describe('PgProductRepository', () => {
+  let prisma: PrismaClient;
+  let sut: PgProductRepository;
+
+  beforeAll(() => {
+    prisma = new PrismaClient();
+  });
+
+  beforeEach(async () => {
+    sut = new PgProductRepository();
+  });
+
+  afterEach(async () => {
+    await prisma.product.deleteMany({});
+  });
+
+  describe('create', () => {
+    it('Should return an user if email exists', async () => {
+      await sut.create({
+        name: 'any_product_name',
+        description: 'any_product_description',
+        price: 10,
+        quantity: 2,
+      });
+
+      const product = await prisma.product.findFirst({
+        where: {
+          name: 'any_product_name',
+        },
+      });
+
+      expect(product?.id).toBeTruthy();
+      expect(product?.name).toBe('any_product_name');
+      expect(product?.description).toBe('any_product_description');
+      expect(JSON.stringify(product?.price)).toBe(JSON.stringify('10'));
+      expect(product?.quantity).toBe(2);
+    });
+  });
+});
