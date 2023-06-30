452c55d0e1be194de1d39ad1a7f1285eebec61dd
Allysson <allyssonmgs@gmail.com>
2023-06-29 21:07:32 -0300

    feat: ensure RequiredLengthValidator returns undefined on success

--- src/application/validation/required-length.ts
+++ src/application/validation/required-length.ts
@@ -3,6 +3,8 @@ import { InvalidRequiredParamError } from '@/application/errors';
 export class RequiredLengthValidator {
   constructor(private readonly length: number, private readonly fieldName: string) {}
   validate(): Error | undefined {
-    return new InvalidRequiredParamError(this.fieldName);
+    if (this.fieldName.length < this.length) {
+      return new InvalidRequiredParamError(this.fieldName);
+    }
   }
 }
