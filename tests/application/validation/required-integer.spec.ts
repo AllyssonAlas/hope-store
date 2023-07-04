0ae34fc9030d129d011e7a70c1b7542aac48462e
Allysson <allyssonmgs@gmail.com>
2023-07-03 21:39:20 -0300

    test: ensure RequiredIntegerValidator returns RequiredParamError on failure

---
+++ tests/application/validation/required-integer.spec.ts
@@ -0,0 +1,36 @@
+import { RequiredIntegerValidator } from '@/application/validation';
+import { RequiredParamError } from '@/application/errors';
+
+describe('RequiredIntegerValidator', () => {
+  it('Should return RequiredParamError if value is empty', () => {
+    const sut = new RequiredIntegerValidator('any_string_value' as any, 'any_field_name');
+
+    const error = sut.validate();
+
+    expect(error).toEqual(new RequiredParamError('any_field_name'));
+  });
+
+  it('Should return RequiredParamError if value is null', () => {
+    const sut = new RequiredIntegerValidator(null as any, 'any_field_name');
+
+    const error = sut.validate();
+
+    expect(error).toEqual(new RequiredParamError('any_field_name'));
+  });
+
+  it('Should return RequiredParamError if value is undefined', () => {
+    const sut = new RequiredIntegerValidator(undefined as any, 'any_field_name');
+
+    const error = sut.validate();
+
+    expect(error).toEqual(new RequiredParamError('any_field_name'));
+  });
+
+  it('Should return RequiredParamError if value is a float', () => {
+    const sut = new RequiredIntegerValidator(10.5, 'any_field_name');
+
+    const error = sut.validate();
+
+    expect(error).toEqual(new RequiredParamError('any_field_name'));
+  });
+});
