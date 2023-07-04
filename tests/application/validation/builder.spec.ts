217fc040ac9a533edc463504eff4e3975d6956a8
Allysson <allyssonmgs@gmail.com>
2023-07-04 20:59:47 -0300

    test: ensure ValidationBuilder required adds validators correctly

--- tests/application/validation/builder.spec.ts
+++ tests/application/validation/builder.spec.ts
@@ -1,13 +1,36 @@
 
-import { RequiredStringValidator, ValidationBuilder } from '@/application/validation';
+import {
+  RequiredStringValidator,
+  RequiredNumberValidator,
+  RequiredIntegerValidator,
+  ValidationBuilder,
+} from '@/application/validation';
 
 describe('ValidationBuilder', () => {
   it('Should return a RequiredStringValidator', () => {
     const validators = ValidationBuilder
       .of({ value: 'any_value', fieldName: 'any_name' })
-      .required()
+      .required('string')
       .build();
 
     expect(validators).toEqual([new RequiredStringValidator('any_value', 'any_name')]);
+  });
+
+  it('Should return a RequiredNumberValidator', () => {
+    const validators = ValidationBuilder
+      .of({ value: 10, fieldName: 'any_name' })
+      .required('number')
+      .build();
+
+    expect(validators).toEqual([new RequiredNumberValidator(10, 'any_name')]);
+  });
+
+  it('Should return a RequiredIntegerValidator', () => {
+    const validators = ValidationBuilder
+      .of({ value: 10, fieldName: 'any_name' })
+      .required('integer')
+      .build();
+
+    expect(validators).toEqual([new RequiredIntegerValidator(10, 'any_name')]);
   });
 });
