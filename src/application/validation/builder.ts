ec45c256f6bb61b52f76c574f45bab432d468140
Allysson <allyssonmgs@gmail.com>
2023-07-04 20:59:11 -0300

    feat: ensure ValidationBuilder required adds validators correctly

--- src/application/validation/builder.ts
+++ src/application/validation/builder.ts
@@ -1,18 +1,29 @@
-import { RequiredStringValidator, Validator } from '@/application/validation';
+import {
+  RequiredStringValidator,
+  RequiredNumberValidator,
+  RequiredIntegerValidator,
+  Validator,
+} from '@/application/validation';
 
 export class ValidationBuilder {
   private constructor(
-    private readonly value: string,
+    private readonly value: any,
     private readonly fieldName: string,
     private readonly validators: Validator[] = [],
   ) {}
 
-  static of({ value, fieldName }: { value: string, fieldName: string }): ValidationBuilder {
+  static of({ value, fieldName }: { value: any, fieldName: string }): ValidationBuilder {
     return new ValidationBuilder(value, fieldName);
   }
 
-  required(): ValidationBuilder {
-    this.validators.push(new RequiredStringValidator(this.value, this.fieldName));
+  required(valitorType: string): ValidationBuilder {
+    if (valitorType === 'string') {
+      this.validators.push(new RequiredStringValidator(this.value, this.fieldName));
+    } else if (valitorType === 'number') {
+      this.validators.push(new RequiredNumberValidator(this.value, this.fieldName));
+    } else if (valitorType === 'integer') {
+      this.validators.push(new RequiredIntegerValidator(this.value, this.fieldName));
+    }
     return this;
   }
 
