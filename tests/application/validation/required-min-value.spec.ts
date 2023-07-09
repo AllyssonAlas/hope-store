499abd491ca8dbff9cf1642c14a5985b7bd27520
Allysson <allyssonmgs@gmail.com>
2023-07-09 16:08:51 -0300

    test: ensure RequiredMinValueValidator returns undefined if value is higher or equal to minValue

--- tests/application/validation/required-min-value.spec.ts
+++ tests/application/validation/required-min-value.spec.ts
@@ -9,4 +9,12 @@ describe('RequiredMinValueValidator', () => {
 
     expect(error).toEqual(new InvalidRequiredParamError('any_field_name'));
   });
+
+  it('Should return undefined if value is higher or equal to minValue', () => {
+    const sut = new RequiredMinValueValidator(1, 1, 'any_field_name');
+
+    const error = sut.validate();
+
+    expect(error).toBeUndefined();
+  });
 });
