55deb7a22465e1930f449fccbbcedc379f69594f
Allysson <allyssonmgs@gmail.com>
2023-07-09 16:08:38 -0300

    feat: ensure RequiredMinValueValidator returns undefined if value is higher or equal to minValue

--- src/application/validation/required-min-value.ts
+++ src/application/validation/required-min-value.ts
@@ -3,6 +3,8 @@ import { InvalidRequiredParamError } from '@/application/errors';
 export class RequiredMinValueValidator {
   constructor(private readonly minValue: number, private readonly value: number, private readonly fieldName: string) {}
   validate(): Error | undefined {
-    return new InvalidRequiredParamError(this.fieldName);
+    if (this.value < this.minValue) {
+      return new InvalidRequiredParamError(this.fieldName);
+    }
   }
 }
