e75d481d8b164b1ec5ddba846de62a40e7344c2c
Allysson <allyssonmgs@gmail.com>
2023-07-17 21:08:34 -0300

    feat: ensure ExpressMiddleware add locals to request

--- src/main/adapters/express-middleware.ts
+++ src/main/adapters/express-middleware.ts
@@ -5,7 +5,12 @@ import { Middleware } from '@/application/middlewares';
 export const adaptExpressMiddleware = (middleware: Middleware): RequestHandler => {
   return async (req, res, next) => {
     const { data, statusCode } = await middleware.handle({ ...req.headers });
-
-    res.status(statusCode).json({ error: data.message });
+    if (statusCode === 200) {
+      const validFields = Object.entries(data).filter((entry) => entry[1]);
+      req.locals = { ...req.locals, ...Object.fromEntries(validFields) };
+      next();
+    } else {
+      res.status(statusCode).json({ error: data.message });
+    }
   };
 };
