b164a3c744a9220c7bdbbab797fbe0f2be0535f7
Allysson <allyssonmgs@gmail.com>
2023-07-18 20:46:19 -0300

    feat: add AuthorizationMiddleware to route

--- src/main/routes/product.ts
+++ src/main/routes/product.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
 import { makeCreateProductController } from '@/main/factories/application/controllers';
-import { adaptExpressRoute as adapt } from '@/main/adapters';
+import { makeAuthorizationMiddleware } from '@/main/factories/application/middlewares';
+import { adaptExpressRoute as adaptRoute, adaptExpressMiddleware as adaptMiddleware } from '@/main/adapters';
 
 export default (router: Router): void => {
-  router.post('/product/create', adapt(makeCreateProductController()));
+  router.post(
+    '/product/create',
+    adaptMiddleware(makeAuthorizationMiddleware('create_product')),
+    adaptRoute(makeCreateProductController(),
+    ));
 };
