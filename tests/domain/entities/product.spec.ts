106e2c4723ba94361487829d514df1e163c1372a
Allysson <allyssonmgs@gmail.com>
2023-08-01 16:48:23 -0300

    test: ensure Product is created with an id

--- tests/domain/entities/product.spec.ts
+++ tests/domain/entities/product.spec.ts
@@ -18,4 +18,24 @@ describe('Product', () => {
       quantity: 1,
     });
   });
+
+  it('Should create a product with an id', () => {
+    const productData = {
+      id: 'any_product_id',
+      name: 'any_product_name',
+      description: 'any_product_description',
+      price: 1,
+      quantity: 1,
+    };
+
+    const sut = new Product(productData);
+
+    expect(sut).toEqual({
+      id: 'any_product_id',
+      name: 'any_product_name',
+      description: 'any_product_description',
+      price: 1,
+      quantity: 1,
+    });
+  });
 });
