0025b2c60b7a97d9c14e4198cacbb87296423efd
Allysson <allyssonmgs@gmail.com>
2023-08-09 20:15:58 -0300

    feat: type api return

--- src/infra/gateways/postal-code-api.ts
+++ src/infra/gateways/postal-code-api.ts
@@ -1,10 +1,19 @@
 import { PostalCodeApi as PostalCode } from '@/domain/contracts/gateways';
 import { HttpGetClient } from '@/infra/contracts/gateways';
 
+type ApiAddress = {
+  cep: string;
+  city: string;
+  neighborhood: string;
+  street: string;
+}
+
 export class PostalCodeApi {
+  private readonly baseUrl = 'https://brasilapi.com.br/api/cep/v2/';
+
   constructor(private readonly httpClient: HttpGetClient) {}
 
   async getAddress({ postalCode }: PostalCode.Input): Promise<void> {
-    await this.httpClient.get({ url: `https://brasilapi.com.br/api/cep/v2/${postalCode}` });
+    await this.httpClient.get<ApiAddress>({ url: this.baseUrl + postalCode });
   }
 }
