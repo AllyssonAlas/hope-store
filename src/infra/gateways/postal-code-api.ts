22b3bc8b3c72b76bb4e32385fa3a771e75d4823d
Allysson <allyssonmgs@gmail.com>
2023-08-09 20:31:58 -0300

    feat: ensure PostalCodeApi returns null if HttpGetClient return an error

--- src/infra/gateways/postal-code-api.ts
+++ src/infra/gateways/postal-code-api.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import { PostalCodeApi as PostalCode } from '@/domain/contracts/gateways';
 import { HttpGetClient } from '@/infra/contracts/gateways';
 
@@ -13,7 +15,15 @@ export class PostalCodeApi {
 
   constructor(private readonly httpClient: HttpGetClient) {}
 
-  async getAddress({ postalCode }: PostalCode.Input): Promise<void> {
-    await this.httpClient.get<ApiAddress>({ url: this.baseUrl + postalCode });
+  async getAddress({ postalCode }: PostalCode.Input): Promise<any> {
+    try {
+      await this.httpClient.get<ApiAddress>({ url: this.baseUrl + postalCode });
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.status && error?.status < 500) {
+        return null;
+      }
+
+      throw error;
+    }
   }
 }
