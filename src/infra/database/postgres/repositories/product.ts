3d9415e2b6eb6eb598330656cdc3ba83d257e429
Allysson <allyssonmgs@gmail.com>
2023-08-10 19:53:39 -0300

    feat: ensure LoadProductsListRepository returns a list of products

--- src/infra/database/postgres/repositories/product.ts
+++ src/infra/database/postgres/repositories/product.ts
@@ -1,11 +1,24 @@
 import { PrismaClient } from '@prisma/client';
 
-import { CreateProductRepository } from '@/domain/contracts/repositories';
+import { CreateProductRepository, LoadProductsListRepository } from '@/domain/contracts/repositories';
 
-export class PgProductRepository implements CreateProductRepository {
+export class PgProductRepository implements CreateProductRepository, LoadProductsListRepository {
   private prisma = new PrismaClient();
 
   async create(input: CreateProductRepository.Input): Promise<void> {
     await this.prisma.product.create({ data: input });
+  }
+
+  async loadList({ ids }: LoadProductsListRepository.Input): Promise<LoadProductsListRepository.Output> {
+    const products = await this.prisma.product.findMany({
+      where: {
+        id: { in: ids },
+      },
+    });
+    const productsFormatted = products.map(product => ({
+      ...product,
+      price: Number(product.price),
+    }));
+    return productsFormatted;
   }
 }
