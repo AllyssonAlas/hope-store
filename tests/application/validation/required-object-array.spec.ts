c5cd63e1568493e3d903febd8d57b81c7f368e11
Allysson <allyssonmgs@gmail.com>
2023-08-22 20:13:03 -0300

    test: ensure RequiredObjectArrayValidator returns InvalidRequiredParamError if value is not an array

---
+++ tests/application/validation/required-object-array.spec.ts
@@ -0,0 +1,12 @@
+import { RequiredObjectArrayValidator } from '@/application/validation';
+import { InvalidRequiredParamError } from '@/application/errors';
+
+describe('RequiredObjectArrayValidator', () => {
+  it('Should return InvalidRequiredParamError if value is not an array', () => {
+    const sut = new RequiredObjectArrayValidator('' as any, 'any_field');
+
+    const error = sut.validate();
+
+    expect(error).toEqual(new InvalidRequiredParamError('any_field'));
+  });
+});
