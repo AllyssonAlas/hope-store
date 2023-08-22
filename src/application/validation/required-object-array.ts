cb75661f9487dcca2f0e66d4e5c6a945fd1af8f9
Allysson <allyssonmgs@gmail.com>
2023-08-22 20:14:27 -0300

    feat: ensure RequiredObjectArrayValidator returns undefined if value is  an array

--- src/application/validation/required-object-array.ts
+++ src/application/validation/required-object-array.ts
@@ -4,6 +4,8 @@ export class RequiredObjectArrayValidator {
   constructor(private readonly value: Array<any>, private readonly fieldName: string) {}
 
   validate(): Error | undefined {
-    return new InvalidRequiredParamError(this.fieldName);
+    if (!Array.isArray(this.value)) {
+      return new InvalidRequiredParamError(this.fieldName);
+    }
   }
 }
