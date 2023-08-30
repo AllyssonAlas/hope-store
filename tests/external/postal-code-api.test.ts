be6de007850f10cfb81625bb7e91e25cc8fc3d2d
Allysson <allyssonmgs@gmail.com>
2023-08-30 20:48:07 -0300

    test: ensure  PostalCodeApi returns correct data on success

---
+++ tests/external/postal-code-api.test.ts
@@ -0,0 +1,23 @@
+import { PostalCodeApi, AxiosHttpClient } from '@/infra/gateways';
+
+describe('Postal Code Api Integration Tests', () => {
+  let axiosClient: AxiosHttpClient;
+  let sut: PostalCodeApi;
+
+  beforeEach(() => {
+    axiosClient = new AxiosHttpClient();
+    sut = new PostalCodeApi(axiosClient);
+  });
+
+  it('Should return the correct address when postal code is valid', async () => {
+    const address = await sut.getAddress({ postalCode: '11722170' });
+
+    expect(address).toEqual({
+      postalCode: '11722170',
+      state: 'SP',
+      city: 'Praia Grande',
+      neighborhood: 'Vila Sônia',
+      street: 'Rua Sérgio Gregório',
+    });
+  });
+});
